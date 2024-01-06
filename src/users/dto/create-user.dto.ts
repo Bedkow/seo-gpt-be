ed8d633cdf14a123a0493df8b8aa196e270dae0a
Bedkow <bednarekdaniel@o2.pl>
2024-01-06 14:35:28 +0100

    add user role validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,11 @@
-import { Length } from "class-validator";
+import { IsEnum, Length } from "class-validator";
 
 export class CreateUserDto {
   id: string;
   @Length(3, 50)
   name: string;
-  role: string;
+  @IsEnum(['none', 'copywriter', 'administrator'], {message: `Role should be 'none', 'copywriter', or 'administrator'`})
+  role: 'none' | 'copywriter' | 'administrator';
   queriesToday: number;
   queriesTotal: number;
 }
