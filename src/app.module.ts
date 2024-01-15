bfe578ef346e1dcec692489cb9b07c6ba422cd4a
Bedkow <bednarekdaniel@o2.pl>
2024-01-15 22:27:59 +0100

    add .env to gitignore, .env.example, update docs, install dotenv lib, connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { QueriesModule } from './queries/queries.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
 
 @Module({
-  imports: [UsersModule, QueriesModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [UsersModule, QueriesModule, MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
