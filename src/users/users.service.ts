30ec7949cdb3b550609e004af260719ba28869e0
Bedkow <bednarekdaniel@o2.pl>
2024-01-18 21:13:45 +0100

    initial mongo services setup

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,42 +7,43 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  private users = [
-    {
-      id: 'xyz-123',
-      name: 'jan kowalski',
-      role: 'copywriter',
-      queriesToday: 6,
-      queriesTotal: 48,
-    },
-    {
-      id: 'zzz-321',
-      name: 'żaneta pąkiewicz',
-      role: 'administrator',
-      queriesToday: 0,
-      queriesTotal: 5,
-    },
-  ];
+  // private users = [
+  //   {
+  //     id: 'xyz-123',
+  //     name: 'jan kowalski',
+  //     role: 'copywriter',
+  //     queriesToday: 6,
+  //     queriesTotal: 48,
+  //   },
+  //   {
+  //     id: 'zzz-321',
+  //     name: 'żaneta pąkiewicz',
+  //     role: 'administrator',
+  //     queriesToday: 0,
+  //     queriesTotal: 5,
+  //   },
+  // ];
   // schemas playground
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
   
-  async createUser_S(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
-    return createdUser.save();
-  }
-
-  async getAllUsers_S(): Promise<User[]> {
+  async getAllUsers(): Promise<User[]> {
     return this.userModel.find().exec();
   }
   
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
   //
 
-  getAllUsers() {
-    return this.users;
-  }
+  // getAllUsers() {
+  //   return this.users;
+  // }
 
-  getUserByName(name: string) {
-    const user = this.users.find((user) => user.name == name);
+  async getUserByName(name: string) {
+    // const user = this.users.find((user) => user.name == name);
+
+    const user = await this.userModel.findOne({_name: name})
 
     if (!user) {
       throw new Error('User Not Found');
@@ -51,10 +52,12 @@ export class UsersService {
     }
   }
 
-  getUserById(userId: string) {
-    const user = this.users.find((user) => {
-      return user.id === userId;
-    });
+  async getUserById(userId: string) {
+    // const user = this.users.find((user) => {
+    //   return user.id === userId;
+    // });
+
+    const user = await this.userModel.findOne({_userId: userId})
 
     if (!user) {
       throw new Error('User Not found');
@@ -63,27 +66,38 @@ export class UsersService {
     }
   }
 
-  createUser(createUserDto: CreateUserDto) {
-    this.users.push(createUserDto);
+  // createUser(createUserDto: CreateUserDto) {
+  //   this.users.push(createUserDto);
+  // }
+
+  async removeUser(userId: string) {
+    // const toBeRemoved = this.getUserById(userId);
+
+    const result = await this.userModel.deleteOne({_userId: userId})
+
+    // this.users = this.users.filter((user) => {
+    //   return user.id !== id;
+    // });
+
+    return result;
   }
 
-  removeUser(id: string) {
-    const toBeRemoved = this.getUserById(id);
+  async updateUser(userId: string, updateUserDto: UpdateUserDto) {
 
-    this.users = this.users.filter((user) => {
-      return user.id !== id;
-    });
-
-    return toBeRemoved;
-  }
-
-  updateUser(id: string, updateUserDto: UpdateUserDto) {
-    this.users = this.users.map((user) => {
-      if (user.id === id) {
-        return { ...user, ...updateUserDto };
+    await this.userModel.findByIdAndUpdate(userId, {...updateUserDto}, (err, result)=> {
+      if(err){
+        return err
+      } else {
+        return result
       }
-      return user;
-    });
-    return this.getUserById(id);
+    })
+
+    // this.users = this.users.map((user) => {
+    //   if (user.id === id) {
+    //     return { ...user, ...updateUserDto };
+    //   }
+    //   return user;
+    // });
+    // return this.getUserById(id);
   }
 }
